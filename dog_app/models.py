5cba31aed85546b407cab480445baa2988d60598
Shari Reyes <shari2289@yahoo.com>
2020-09-25 02:16:33 -0500

    added model

--- dog_app/models.py
+++ dog_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model): 
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    characteristics = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+        
+
+
+    
--- dog_app/views.py
+++ dog_app/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Dog
 
-class Dog: 
-  def __init__(self, name, breed, size, color, characteristics, age):
-    self.name = name
-    self.breed = breed
-    self.size = size
-    self.color = color
-    self.characteristics = characteristics
-    self.age = age
-
-dogs = [
-  Dog('Kobe', 'maltese', 'small', 'black', 'high energy', 0),
-  Dog('Bailey', 'maltipoo', 'medium', 'brown', 'sweet nad mild tempered', 1),
-  Dog('JoJo', 'labrador', 'large', 'golden', 'loyal, likes to play fetch', 3),
-]
 
 def home(request):
-  return HttpResponse('<h1>Hello Welcome To: The Dog Collector')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })    
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
